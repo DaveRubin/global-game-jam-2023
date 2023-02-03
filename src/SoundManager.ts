f8c650d3d37e8a21c91017da9dc5ad3da5cabaf7
David Rubin <dudrubin@gmail.com>
2023-02-03 19:07:00 +0200

    Added music

--- src/SoundManager.ts
+++ src/SoundManager.ts
@@ -1,10 +1,24 @@
-import { Engine, Sound } from "@babylonjs/core";
+import { Sound } from "@babylonjs/core";
 import { animateTo } from "./animations";
+
+const DIG_VOLUME = 0.1;
+const MUSIC_VOLUME = 0.3;
 
 export class SoundMananger {
   dig: Sound;
+  music: Sound;
+
   constructor() {
-    this.dig = new Sound("dig", "./sounds/dig.mp3");
+    this.dig = new Sound("dig", "./sounds/dig.mp3", undefined, () => {}, { loop: true, volume: DIG_VOLUME });
+    this.music = new Sound(
+      "music",
+      "./sounds/music.mp3",
+      undefined,
+      () => {
+        this.music.play();
+      },
+      { loop: true, volume: MUSIC_VOLUME }
+    );
     this.dig.loop = true;
   }
   startDig = () => {
@@ -12,14 +26,14 @@ export class SoundMananger {
     const gain = this.dig.getSoundGain();
     if (gain) {
       gain.gain.value = 0;
-      animateTo(gain.gain, "value", 1, [0, 1]);
+      animateTo(gain.gain, "value", 1, [0, DIG_VOLUME]);
     }
   };
   stopDig = () => {
     const gain = this.dig.getSoundGain();
 
     if (gain) {
-      animateTo(gain.gain, "value", 1, [1, 0]).then(() => {
+      animateTo(gain.gain, "value", 1, [DIG_VOLUME, 0]).then(() => {
         this.dig.stop();
       });
     }
